2d21cd61f8a86a231434d32c3357d5f5d0731a48
Evan <43254406+evan-dg31@users.noreply.github.com>
2024-06-30 04:12:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,4 @@ overall_total_profit = df['Profit'].sum()
 overall_profit_margin = overall_total_profit/overall_total_sales * 100 if overall_total_sales !=0 else 0
 profit_margin_delta = profit_margin - overall_profit_margin
 
-st.metric(label='Profit Margin',value=f"{profit_margin_delta:,.2f}", delta=f"{overall_profit_margin:.2f}%")
+st.metric(label='Profit Margin Delta',value=f"{profit_margin_delta:,.2f}", delta=f"{overall_profit_margin:.2f}%")
